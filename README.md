34ac8d990871ac034a26c52ea481ce281611386c
baye94 <fallcheikh893@gmail.com>
2022-04-16 02:24:51 +0000

    printf

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-0x11. C - printf
-
+# 0x11. C - printf
+Foundations - Low-level programming & Algorithm ― Hatching out
+by Julien Barbier, co-founder at Holberton School.
+Project to be done in teams of 2 people (your team: Baye cheikh fall, Mairam Abdoul Niane
 
--- main.h
+++ main.h
@@ -2,24 +2,35 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * struct format_func - Struct with format with its respective function
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
  *
- * @ft: The format
- * @func: The function associated
+ * Return: int
  */
-typedef struct format_func
+typedef struct structprint
 {
-	char *ft;
-	int (*func)();
-} format_func;
+	char *q;
+	int (*u)(char *format, va_list);
+} structype;
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int p_char(va_list);
-int p_str(va_list);
-int p_int(va_list);
-int p_percent(va_list);
-
-#endif /* main_H */
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
+#endif
