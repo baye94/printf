aa32104eb1a31bf5fbdf37dcfab6d56f0d5bdafb
baye94 <fallcheikh893@gmail.com>
2022-04-16 01:50:19 +0000

    printf

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <limits.h>
+
+/**
+ * struct format_func - Struct with format with its respective function
+ *
+ * @ft: The format
+ * @func: The function associated
+ */
+typedef struct format_func
+{
+	char *ft;
+	int (*func)();
+} format_func;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int p_char(va_list);
+int p_str(va_list);
+int p_int(va_list);
+int p_percent(va_list);
+
+#endif /* main_H */
